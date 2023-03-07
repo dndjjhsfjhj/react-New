3e8ffa0675aea51f24c5564eda31f29309327b77
dndjjhsfjhj <92665212+dndjjhsfjhj@users.noreply.github.com>
2023-03-07 17:15:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-New
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-gnwbh6)
---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <table>
+        <thead>
+           <td>
+             <img src=/>
+           </td>
+           <td>
+             komal
+           </td>
+           <td>
+             komal
+           </td>
+           <td>
+             komal
+           </td>
+           <td>
+             komal
+           </td>
+           <td>
+             komal
+           </td>
+           <td>
+             komal
+           </td>
+          </thead>
+        </table>
+    </div>
+  );
+}
